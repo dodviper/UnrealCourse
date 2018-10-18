2a9ae9489ca95fcb8b621fdb589f4172f2665017
DESKTOP-7565T4D\Hegemon <konviktor93@gmail.com>
2018-10-18 22:45:50 +0200

    Find screensize

--- BattleTank/BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -2,7 +2,6 @@
 
 #include "TankPlayerController.h"
 
-
 void ATankPlayerController::BeginPlay() {
 	Super::BeginPlay();
 
@@ -34,8 +33,31 @@ ATank* ATankPlayerController::GetControlledTank() const{
 void ATankPlayerController::AimTowardsCrosshair() {
 	if (!GetControlledTank()) { return; }
 
+	FVector HitLocation; // out parameter
+
+	if (GetSightRayHitLocation(HitLocation)) { 
+		//UE_LOG(LogTemp, Warning, TEXT("Look direction: %s"), *HitLocation.ToString());
+		// TODO tell controlled tank to aim at this point
+	}
+
+
 	// Get world location if linetrace through crosshair
 	// If it hits the landscape
 	// tell controlled tank to aim at this point
 
+}
+
+
+// if it hits the landscape through crosshair, we return true
+bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const {
+	// Find the crosshair position
+	// de-project the screen position of the crosshair to a world direction
+	// linetrace along that look direction and see what we hit (up to some max range)
+
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+
+	auto ScreenLocation = FVector2D(ViewportSizeX * CrosshairXLocation , ViewportSizeY * CrosshairYLocation);
+	
+	return true;
 }
--- BattleTank/BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/BattleTank/Source/BattleTank/TankPlayerController.h
@@ -16,7 +16,7 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
-public:
+private:
 	ATank* GetControlledTank() const;
 	
 	virtual void BeginPlay() override;
@@ -25,4 +25,14 @@ public:
 	// start the tank moving the barrel so that a shot would hit where
 	// the crosshair intersects the world
 	void AimTowardsCrosshair();
+	bool GetSightRayHitLocation(FVector& HitLocation) const; // true if hit landscape
+
+
+	UPROPERTY(EditAnywhere)
+		float CrosshairXLocation = 0.5f;
+	UPROPERTY(EditAnywhere)
+		float CrosshairYLocation = 0.33333f;
+
+
+
 };
